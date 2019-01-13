47bc48fbf6001cf2960c47324c872603d6404ac8
Tomas Barak <baryhoemail@gmail.com>
2019-01-13 15:16:55 +0100

    * changed outfile name

--- Src/Program.cs
+++ Src/Program.cs
@@ -7,13 +7,14 @@ namespace GeneratorDanovehoPriznani
 		static void Main(string[] args)
 		{
 			var inPath = "../../dph3-in.xml";
-			var outPath = "../../dph3.xml";
 
 			var dph3 = DPH3.Pisemnost.FromXml(inPath);
 			var ctx = new GeneratorContext();
 			ctx.FetchData();
 
 			dph3.Generate(ctx);
+
+			var outPath = string.Format("../../{0}-dph3.xml", ctx.PeriodMonthCode);
 			dph3.ToXml(outPath);
 		}
 	}
