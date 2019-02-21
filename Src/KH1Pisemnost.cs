48c0f15573b337c3c981c867372d634e0f9534a8
Tomas Barak <baryhoemail@gmail.com>
2019-02-21 18:59:29 +0100

    KH1 VetaC working

--- Src/KH1Pisemnost.cs
+++ Src/KH1Pisemnost.cs
@@ -1,6 +1,8 @@
 ﻿// Author: Tomas Barak
 
+using System;
 using System.IO;
+using System.Linq;
 using System.Xml.Serialization;
 
 namespace GeneratorDanovehoPriznani.KH1
@@ -35,6 +37,14 @@ namespace GeneratorDanovehoPriznani.KH1
 		public void Generate(GeneratorContext ctx)
 		{
 			VetaD?.Generate(ctx);
+			VetaA1 = null;
+			VetaA2 = null;
+			VetaA3 = null;
+			VetaA4 = null;
+			VetaB1 = null;
+			VetaB2 = null;
+			VetaC = new PisemnostDPHKH1VetaC();
+			VetaC.Generate(ctx);
 		}
 	}
 
@@ -45,6 +55,26 @@ namespace GeneratorDanovehoPriznani.KH1
 			mesic = ctx.Period.Month;
 			rok = ctx.Period.Year;
 			d_poddp = ctx.SubmitDate.ToString("d.M.yyyy");
+		}
+	}
+
+	public partial class PisemnostDPHKH1VetaC
+	{
+		public void Generate(GeneratorContext ctx)
+		{
+			var incommingValue =
+				from t in ctx.Transactions
+				where t.Direction == Transaction.EDirection.Incoming
+				select t.Value;
+			obrat23 = Math.Round(incommingValue.Sum());
+			obrat23Specified = true;
+
+			var outgoingValue =
+				from t in ctx.Transactions
+				where t.Direction == Transaction.EDirection.Outgoing
+				select t.Value;
+			pln23 = Math.Round(outgoingValue.Sum());
+			pln23Specified = true;
 		}
 	}
 }
