bb4bd0b3ca10572d4c9a3f8778f965824338c788
Tomas Barak <baryhoemail@gmail.com>
2019-02-21 18:41:02 +0100

    + kontrolni hlaseni WIP

--- Src/KH1Pisemnost.cs
+++ Src/KH1Pisemnost.cs
@@ -1,8 +1,50 @@
 ﻿// Author: Tomas Barak
 
+using System.IO;
+using System.Xml.Serialization;
+
 namespace GeneratorDanovehoPriznani.KH1
 {
 	public partial class Pisemnost
 	{
+		public static Pisemnost FromXml(string fileName)
+		{
+			var serializer = new XmlSerializer(typeof(Pisemnost));
+			var reader = new StreamReader(fileName);
+			var kh1 = (Pisemnost)serializer.Deserialize(reader);
+			return kh1;
+		}
+
+		public void ToXml(string fileName)
+		{
+			var serializer = new XmlSerializer(typeof(Pisemnost));
+			var writer = new StreamWriter(fileName);
+			serializer.Serialize(writer, this);
+		}
+
+		public void Generate(GeneratorContext ctx)
+		{
+			nazevSW = ctx.AppName;
+			verzeSW = ctx.AppVersion;
+			DPHKH1?.Generate(ctx);
+		}
+	}
+
+	public partial class PisemnostDPHKH1
+	{
+		public void Generate(GeneratorContext ctx)
+		{
+			VetaD?.Generate(ctx);
+		}
+	}
+
+	public partial class PisemnostDPHKH1VetaD
+	{
+		public void Generate(GeneratorContext ctx)
+		{
+			mesic = ctx.Period.Month;
+			rok = ctx.Period.Year;
+			d_poddp = ctx.SubmitDate.ToString("d.M.yyyy");
+		}
 	}
 }
--- Src/Program.cs
+++ Src/Program.cs
@@ -4,18 +4,35 @@ namespace GeneratorDanovehoPriznani
 {
 	class Program
 	{
-		static void Main(string[] args)
+		static void ProcessDPH3(GeneratorContext ctx)
 		{
 			var inPath = "../../dph3-in.xml";
-
 			var dph3 = DPH3.Pisemnost.FromXml(inPath);
-			var ctx = new GeneratorContext();
-			ctx.FetchData();
 
 			dph3.Generate(ctx);
 
 			var outPath = string.Format("../../{0}-dph3.xml", ctx.PeriodMonthCode);
 			dph3.ToXml(outPath);
+		}
+
+		static void ProcessKH1(GeneratorContext ctx)
+		{
+			var inPath = "../../kh1-in.xml";
+			var kh1 = KH1.Pisemnost.FromXml(inPath);
+
+			kh1.Generate(ctx);
+
+			var outPath = string.Format("../../{0}-kh1.xml", ctx.PeriodMonthCode);
+			kh1.ToXml(outPath);
+		}
+
+		static void Main(string[] args)
+		{
+			var ctx = new GeneratorContext();
+			ctx.FetchData();
+
+			ProcessDPH3(ctx);
+			ProcessKH1(ctx);
 		}
 	}
 }
